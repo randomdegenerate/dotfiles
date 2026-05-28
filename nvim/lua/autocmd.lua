b8fbab9c0c4323ff799ad5965ef50b3e47d5abcc
randomdegenerate <sandilkosgahakumbura@gmail.com>
2026-05-28 20:32:37 +0100

    updated nvim and term

---
+++ nvim/lua/autocmd.lua
@@ -0,0 +1,25 @@
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("lsp", { clear = true }),
+  callback = function(args)
+    -- 2
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      -- 3
+      buffer = args.buf,
+      callback = function()
+        -- 4 + 5
+        vim.lsp.buf.format {async = false, id = args.data.client_id }
+      end,
+    })
+  end
+})
+
+vim.api.nvim_create_autocmd('TermOpen', {
+  group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
+  callback = function()
+    vim.opt.number = false
+    vim.opt.relativenumber = false
+  end,
+})
+
+
---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,104 @@
+vim.pack.add({
+    { src = "https://github.com/mplusp/pack-manager.nvim"},
+    { src = "https://github.com/catppuccin/nvim"},
+    { src = "https://github.com/blazkowolf/gruber-darker.nvim"},
+    { src = "https://github.com/bluz71/vim-moonfly-colors"},
+    { src = "https://github.com/nyoom-engineering/oxocarbon.nvim"},
+    { src = "https://github.com/norcalli/nvim-colorizer.lua"},
+    { src = "https://github.com/nvim-lualine/lualine.nvim"},
+    { src = 'https://github.com/nvim-tree/nvim-web-devicons' },
+    { src = 'https://github.com/nvim-tree/nvim-tree.lua' },
+    { src = "https://github.com/neovim/nvim-lspconfig" },
+    { src = "https://github.com/L3MON4D3/LuaSnip"},
+    { src = "https://github.com/rafamadriz/friendly-snippets"},
+    { src = "https://github.com/saghen/blink.cmp"},
+    { src = "https://github.com/mfussenegger/nvim-jdtls"},
+    { src = "https://github.com/nvim-mini/mini.pick"},
+    { src = "https://github.com/stevearc/oil.nvim"},
+    { src = "https://github.com/ThePrimeagen/refactoring.nvim"},
+    { src = "https://github.com/norcalli/nvim-colorizer.lua"},
+})
+
+-- THEMES
+-- catppuccin-macchiato/mocha
+-- nightfly 
+vim.cmd("colorscheme oxocarbon")
+
+require('mini.pick').setup()
+-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+
+require('lualine').setup {
+  options = { theme = 'palenight'}
+}
+
+require('colorizer').setup()
+
+require('luasnip.loaders.from_vscode').load{ exclude = {},}
+
+require("blink.cmp").setup({
+    fuzzy = { implementation = "lua" },
+    signature = { enabled = true },
+    keymap = {
+        preset = "default",
+        ["<C-space>"] = {},
+        ["<C-p>"] = {},
+        ["<Tab>"] = {},
+        ["<S-Tab>"] = {},
+        ["<C-n>"] = { "show", "show_documentation", "hide_documentation" },
+        ["<C-y>"] = { "select_and_accept" },
+        ["<C-k>"] = { "select_prev", "fallback" },
+        ["<C-j>"] = { "select_next", "fallback" },
+        ["<C-b>"] = { "scroll_documentation_down", "fallback" },
+        ["<C-f>"] = { "scroll_documentation_up", "fallback" }, ["<C-l>"] = { "snippet_forward", "fallback" },
+        ["<C-h>"] = { "snippet_backward", "fallback" },
+        ["<C-e>"] = { "hide" },
+    },
+
+    appearance = {
+        use_nvim_cmp_as_default = true,
+        nerd_font_variant = "normal",
+    },
+
+    completion = {
+        documentation = {
+            auto_show = false,
+            auto_show_delay_ms = 250,
+        },
+    },
+
+    cmdline = {
+        keymap = {
+            preset = "inherit",
+            ["<CR>"] = { "accept_and_enter", "fallback" },
+        },
+    },
+
+    sources = { default = { "lsp" } },
+})
+
+require('oil').setup()
+--require('nvim-tree').setup({
+--      sort = {
+--        sorter = "case_sensitive",
+--      },
+--
+--      view = {
+--        width = 25,
+--      },
+--
+--      renderer = {
+--        group_empty = true,
+--      },
+--
+--      filters = {
+--        dotfiles = true,
+--      },
+--})
+
+
+
+
+
+
