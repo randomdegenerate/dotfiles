a1a8fec2023fcb84e6f40f4743680865a9845e30
randomdegenerate <sandilkosgahakumbura@gmail.com>
2026-06-04 22:20:57 +0100

    config updated

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -40,41 +40,15 @@ require('luasnip.loaders.from_vscode').load{ exclude = {},}
 require("blink.cmp").setup({
     fuzzy = { implementation = "lua" },
     signature = { enabled = true },
-    keymap = {
-        preset = "default",
-        ["<C-space>"] = {},
-        ["<C-p>"] = {},
-        ["<Tab>"] = {},
-        ["<S-Tab>"] = {},
-        ["<C-n>"] = { "show", "show_documentation", "hide_documentation" },
-        ["<C-y>"] = { "select_and_accept" },
-        ["<C-k>"] = { "select_prev", "fallback" },
-        ["<C-j>"] = { "select_next", "fallback" },
-        ["<C-b>"] = { "scroll_documentation_down", "fallback" },
-        ["<C-f>"] = { "scroll_documentation_up", "fallback" }, ["<C-l>"] = { "snippet_forward", "fallback" },
-        ["<C-h>"] = { "snippet_backward", "fallback" },
-        ["<C-e>"] = { "hide" },
-    },
-
-    appearance = {
-        use_nvim_cmp_as_default = true,
-        nerd_font_variant = "normal",
-    },
-
     completion = {
+        menu = {
+            auto_show = false,
+        },
         documentation = {
             auto_show = false,
             auto_show_delay_ms = 250,
         },
     },
-
-    cmdline = {
-        keymap = {
-            preset = "inherit",
-            ["<CR>"] = { "accept_and_enter", "fallback" },
-        },
-    },
-
     sources = { default = { "lsp" } },
 })
 
